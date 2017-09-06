241b245f0a52a72ba4701344112024dd0a9c1e25
Wilco de Boer <wfh@wilcodeboer.me>
2017-09-06 21:02:10 +0200

    Add a working favicon

--- startup/both/flowrouter-config.js
+++ startup/both/flowrouter-config.js
@@ -2,9 +2,5 @@ SEO = new FlowRouterSEO();
 
 SEO.setDefaults({
   title: 'ScienceShip',
-  description: 'TODO',
-  author: 'Wilco de Boer | Wicloz',
-  meta: {
-    'rel="icon"': Meteor.settings.public.favicon
-  }
+  description: 'TODO'
 });
