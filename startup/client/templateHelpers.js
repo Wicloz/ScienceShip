7000905237d789178b5edb46ac96da3c7ea669ba
Wilco de Boer <wfh@wilcodeboer.me>
2017-09-07 23:27:52 +0200

    Create several string helpers

--- startup/client/templateHelpers.js
+++ startup/client/templateHelpers.js
@@ -1,5 +1,3 @@
 Template.registerHelper('titleFor', (routeName) => {
-  routeName = routeName.replace(/([A-Z])/g, ' $1');
-  routeName = routeName.charAt(0).toUpperCase() + routeName.slice(1);
-  return routeName;
+  return routeName.humanize();
 });
